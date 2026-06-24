b14ea6359ba5baa65ac93cf7541a0ad5053a0c5f
Vignesh21102 <s2025207037@gmail.com>
2026-06-24 19:41:25 +0530

    Update database.py

--- database.py
+++ database.py
@@ -1,1 +1,21 @@
+import sqlite3
 
+
+def connect_db():
+
+    conn = sqlite3.connect("database/inventory.db")
+
+    cursor = conn.cursor()
+
+    cursor.execute("""
+    CREATE TABLE IF NOT EXISTS products(
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        name TEXT NOT NULL,
+        quantity INTEGER NOT NULL,
+        price REAL NOT NULL
+    )
+    """)
+
+    conn.commit()
+
+    return conn
